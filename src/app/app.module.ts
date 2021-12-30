045b501dbdd3ae7a38d630368fcee7d815d62d90
Piotr Radlak <p10trek@o2.pl>
2021-12-30 22:18:03 +0100

    added components profile and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
-import { ProvidersComponent } from './providers/providers.component';
+import { RegisterComponent } from './register/register.component';
 import { ServicesListComponent } from './servicesList/servicesList.component';
 import { ServicesListFormsComponent } from './servicesList/servicesList-forms/servicesList-forms.component';
-import { ProviderFormsComponent } from './providers/provider-forms/provider-forms.component';
+import { RegisterFormsComponent } from './register/register-forms/register-forms.component';
 import { RouterModule } from '@angular/router';
 import { ClientsComponent } from './clients/clients.component';
 import { ClientsFormsComponent } from './clients/clients-forms/clients-forms.component';
@@ -28,14 +28,16 @@ import { simpleReducer } from './simple.reducer';
 import { StringReducer } from './string.reducer';
 import { userReducer } from './user.reducer';
 import { ServicesServiceList } from './shared/servicesList.service';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileFormsComponent } from './profile/profile-forms/profile-forms.component';
 export function tokenGetter(){
   return localStorage.getItem("jwt");
 }
 @NgModule({
   declarations: [
     AppComponent,
-    ProvidersComponent,
-    ProviderFormsComponent,
+    RegisterComponent,
+    RegisterFormsComponent,
     ClientsComponent,
     ClientsFormsComponent,
     OrdersComponent,
@@ -50,6 +52,8 @@ export function tokenGetter(){
     HomeFormComponent,
     MenuComponent,
     MenuFormComponent,
+    ProfileComponent,
+    ProfileFormsComponent
   ],
   imports: [
     BrowserModule,
@@ -58,11 +62,12 @@ export function tokenGetter(){
     StoreModule.forRoot({userName:StringReducer,user:userReducer}
     ),
     RouterModule.forRoot([
-      {path: 'providers', component: ProvidersComponent, canActivate:[AuthGuardService]},
+      {path: 'register', component: RegisterComponent},
       {path: 'clients', component: ClientsComponent},
-      {path: 'services', component: ServicesComponent},
+      {path: 'services', component: ServicesComponent, canActivate:[AuthGuardService]},
+      {path: 'profile', component: ProfileComponent, canActivate:[AuthGuardService]},
       {path: 'login', component: LoginComponent},
-      {path: 'servicesList', component: ServicesListComponent},
+      {path: 'servicesList', component: ServicesListComponent, canActivate:[AuthGuardService]},
     ]),
     JwtModule.forRoot({
       config: {
---
+++ src/app/shared/register.model.ts
@@ -0,0 +1,14 @@
+export class Register {
+    Login:string='';
+    Password:string='';
+    Name:string='';
+    Email:string='';
+    Phone:string='';
+    Description:string='';
+    Logo:string='';
+    City:string='';
+    Street:string='';
+    Number:string='';
+    PostalCode:string='';
+    IsProvider:boolean=false;
+}
